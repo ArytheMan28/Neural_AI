808e2c45b395ffc7e9bc0fc1e6ff3043a6cd50d5
ArytheMan28 <aryamanbothra@gmail.com>
2025-08-17 13:58:52 -0500

    Update keys.js

--- static/keys.js
+++ static/keys.js
@@ -1,8 +1,7 @@
-export const hugging_face_key = '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'
+export const hugging_face_key = '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'
 export const cloudinaryCloudname = "dmm8zr0az"
 export const sightengine_api_user = "491630136"
 export const sightengine_api_secret = "C4MoxqQPspV7jVvMv728pcy2sTpjDe9A"
 export const sightengine_workflow = "wfl_g5fdiHz4maLRX7ezhd867"
 export const sightengine_listID = "tli_g5t6wWqDfOy6QW00gUBbQ"
 export const elevenlabs_api_key = "sk_bcaec6b779e279d180e2b3197cecbd0f08d5795822bb4f3b"
-
